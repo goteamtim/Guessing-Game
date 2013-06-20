3fd6f0f8803fe11039714c502d223d5bab394752
Tim Harshman <goteamtim+git@gmail.com>
2013-06-20 08:57:22 -0600

    working version
    Just did some cleanup

--- numGame.js
+++ numGame.js
@@ -1,32 +1,34 @@
-var result = "test";
+result = "test";
 //Initialize the winning number
     var winningNum = Math.floor(Math.random()*11);
-    var guesses = 0;
+    
 function gameLogic() {
-console.log(winningNum);
+guesses = 0;
 
     do {
         guess = prompt("What is your guess?");
+        //If something other than a number is entered, it fails and asks again.
         if (isNaN(guess)) {
             guess = prompt("What is your guess?", "Please enter a number.");
             break;
         }
-
         if (guess > winningNum) {
-            var result = "Your guess is too high.";
+            result = "Your guess is too high.";
             console.log(result);
+            document.getElementById('result').innerHTML = result;
             guesses++;
         } else if (guess < winningNum) {
-            var result = "Your guess is too low.";
+            result = "Your guess is too low.";
             console.log(result);
+            document.getElementById('result').innerHTML = result;
             guesses++;
         } else {
             result = "Congrats, you won!";
             console.log(result);
+            document.getElementById('result').innerHTML = result;
             break;
         }
         
     }
     while (guesses < 5);
-}
-;
+};

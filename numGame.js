cb3199213a119959238e995b96a9cac7c68d8dc0
Tim Harshman <goteamtim+git@gmail.com>
2013-06-22 10:50:54 -0600

    mostly working with div clicks

--- numGame.js
+++ numGame.js
@@ -6,28 +6,78 @@ $(document).ready(function() {
         console.log(guess);
         gameLogic();
     });
+    $("#2").click(function() {
+        $(this).fadeTo("fast", .33);
+        guess = 2;
+        $(this).off();
+        console.log(guess);
+        gameLogic();
+    });
+    $("#3").click(function() {
+        $(this).fadeTo("fast", .33);
+        guess = 3;
+        $(this).off();
+        console.log(guess);
+        gameLogic();
+    });
+    $("#4").click(function() {
+        $(this).fadeTo("fast", .33);
+        guess = 4;
+        $(this).off();
+        console.log(guess);
+        gameLogic();
+    });
+    $("#5").click(function() {
+        $(this).fadeTo("fast", .33);
+        guess = 5;
+        $(this).off();
+        console.log(guess);
+        gameLogic();
+    });
+    $("#6").click(function() {
+        $(this).fadeTo("fast", .33);
+        guess = 6;
+        $(this).off();
+        console.log(guess);
+        gameLogic();
+    });
+    $("#7").click(function() {
+        $(this).fadeTo("fast", .33);
+        guess = 7;
+        $(this).off();
+        console.log(guess);
+        gameLogic();
+    });
+    $("#8").click(function() {
+        $(this).fadeTo("fast", .33);
+        guess = 8;
+        $(this).off();
+        console.log(guess);
+        gameLogic();
+    });
+    $("#9").click(function() {
+        $(this).fadeTo("fast", .33);
+        guess = 9;
+        $(this).off();
+        console.log(guess);
+        gameLogic();
+    });
+    $("#10").click(function() {
+        $(this).fadeTo("fast", .33);
+        guess = 10;
+        $(this).off();
+        console.log(guess);
+        gameLogic();
+    });
 
 });
 var guess;
 result = "test";
 //Initialize the winning number
-var winningNum = Math.floor(Math.random() * 11);
+var winningNum = Math.floor(Math.random() * 10) + 1;
 guesses = 5;
 function gameLogic() {
-
-    console.log(winningNum);
-
-    do {
-        //guess = prompt("What is your guess?");
-//        //If something other than a number is entered, it fails and asks again.
-//        Eventually this will be replaced by another function all together because
-//        that function will collect the status of the div's and then collect input from a click and \n\
-//        return an input.
-        //if (isNaN(guess)) {
-        //    guess = prompt("What is your guess?", "Please enter a number.");
-        //     break;
-        //  }
-        //divGuess();
+    if (guesses > 0) {
         if (guess > winningNum) {
             result = "Your guess is too high.";
             document.getElementById('result').innerHTML = result;
@@ -41,37 +91,12 @@ function gameLogic() {
         } else {
             result = "Congrats, you won! Wanna play again?";
             document.getElementById('result').innerHTML = result;
-            break;
+            //break;
         }
+    } else {
+        alert("Restart game.");
     }
-    while (guesses > 0);
 }
-;
 
-one = 0;
-two = 0;
-three = 0;
-four = 0;
-five = 0;
-six = 0;
-seven = 0;
-eight = 0;
-nine = 0;
-ten = 0;
 
-function divGuess() {
-    if ($("#1").click()) {
-        guess = 1;
-        console.log(guess);
-        return guess;
-
-    } else if ($("#2").click()) {
-        guess = 2;
-        console.log(guess);
-        return guess;
-    }
-    ;
-
-}
-;
 

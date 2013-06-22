f86a9f9be51bb460fdfec8ae4e98e806bcb0a99b
Tim Harshman <goteamtim+git@gmail.com>
2013-06-22 10:41:42 -0600

    before change to jQuery

--- numGame.js
+++ numGame.js
@@ -1,29 +1,33 @@
 $(document).ready(function() {
-    $("td").click(function() {
+    $("#1").click(function() {
         $(this).fadeTo("fast", .33);
+        guess = 1;
+        $(this).off();
+        console.log(guess);
+        gameLogic();
     });
-});
 
-//    $("#start").click(gameLogic());
-//    $("#reset").click(window.location.reload());
+});
+var guess;
 result = "test";
 //Initialize the winning number
 var winningNum = Math.floor(Math.random() * 11);
-
+guesses = 5;
 function gameLogic() {
-    guesses = 5;
+
     console.log(winningNum);
 
     do {
-        guess = prompt("What is your guess?");
+        //guess = prompt("What is your guess?");
 //        //If something other than a number is entered, it fails and asks again.
 //        Eventually this will be replaced by another function all together because
 //        that function will collect the status of the div's and then collect input from a click and \n\
 //        return an input.
-        if (isNaN(guess)) {
-            guess = prompt("What is your guess?", "Please enter a number.");
-            break;
-        }
+        //if (isNaN(guess)) {
+        //    guess = prompt("What is your guess?", "Please enter a number.");
+        //     break;
+        //  }
+        //divGuess();
         if (guess > winningNum) {
             result = "Your guess is too high.";
             document.getElementById('result').innerHTML = result;
@@ -36,9 +40,7 @@ function gameLogic() {
             document.getElementById('guessLeft').innerHTML = guesses;
         } else {
             result = "Congrats, you won! Wanna play again?";
-
             document.getElementById('result').innerHTML = result;
-
             break;
         }
     }
@@ -46,7 +48,30 @@ function gameLogic() {
 }
 ;
 
+one = 0;
+two = 0;
+three = 0;
+four = 0;
+five = 0;
+six = 0;
+seven = 0;
+eight = 0;
+nine = 0;
+ten = 0;
+
 function divGuess() {
-    
-    
-};
+    if ($("#1").click()) {
+        guess = 1;
+        console.log(guess);
+        return guess;
+
+    } else if ($("#2").click()) {
+        guess = 2;
+        console.log(guess);
+        return guess;
+    }
+    ;
+
+}
+;
+
